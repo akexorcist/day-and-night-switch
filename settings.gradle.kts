3311c0fd31cd12f59f7e9753f897f1fbdc79c977
Akexorcist <akexorcist@gmail.com>
2024-06-04 11:37:30 +0700

    Init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "sun-and-moon-switch"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":composeApp")
+//include(":library")
